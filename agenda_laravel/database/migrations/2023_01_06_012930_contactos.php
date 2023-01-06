e481fcfe6caa61a9a47eaf06f77b3bd35d225d07
memeTorres1511 <meme.torres.1511@gmail.com>
2023-01-05 19:38:58 -0600

    migracion lista

--- agenda_laravel/database/migrations/2023_01_06_012930_contactos.php
+++ agenda_laravel/database/migrations/2023_01_06_012930_contactos.php
@@ -13,10 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        //
+        Schema::create('contactos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nombre');
+            $table->string('direccion');
+            $table->string('telefono');
+            $table->string('correo');
+            $table->timestamps();
         });
     }
 
@@ -27,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        //
     }
 };
